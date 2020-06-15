9af32553998ea551f1f3138ab289c2ba7a29ff20
Iron Heart <66810347+IronHeart2000@users.noreply.github.com>
2020-06-15 14:15:27 -0300

    Create Caracteres ASCII.c

---
+++ Caracteres ASCII.c
@@ -0,0 +1,8 @@
+//Exibindo caracteres no console utilizando o especificador de formato %c para cada caractere e os valores decimais correspondentes na tabela ASCII
+
+#include <stdio.h> 
+
+int main(void) { //Função principal
+	printf("%c%c%c%c%c%c%c%c%c%c%c%c", 72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33); //Exibe Hello World! no console
+	return 0; 
+} 
