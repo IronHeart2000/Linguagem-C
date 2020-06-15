4a26f6818cf037c3a5139326e8e2f63b0cdd19fc
Iron Heart <66810347+IronHeart2000@users.noreply.github.com>
2020-06-15 14:10:17 -0300

    Create Primeiro Programa.c

---
+++ Primeiro Programa.c
@@ -0,0 +1,6 @@
+#include <stdio.h> //Inclui todo o conteúdo do arquivo de cabeçalho "stdio.h" no código-fonte
+
+int main(void) { //Função principal
+	printf("Hello World!"); //Exibe a mensagem Hello World! na tela
+	return 0; //Retorna 0 para o sistema operacional
+} //Chave que fecha o bloco da função principal
